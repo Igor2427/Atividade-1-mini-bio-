a4d2921a7d24e3fe39939314bef36defd2fe8ee8
Igor2427 <128043201+Igor2427@users.noreply.github.com>
2025-08-20 19:33:49 +0000

    Mini_bio.1

---
+++ mini_bio/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <h1>
+      <img src="/assets/images/Foto.jpg" width="500" height="500" />
+      <table border="1">
+        <caption> Mini-Biografia </caption>
+        <tr><th> Nome </th><th> idade</th></tr>
+        <tr><th> Igor G </th><th> 20</th></tr>
+        <tr> <th>Nasci no estado de Pernanbuco na cidade de Recife,gosto de quebra cabeças, video-games e Ler, sou mais reservado e as vezes me perco nos meus pensamentos.</th> </tr>
+
+      </table>
+    </h1>
+
+  );
+}
