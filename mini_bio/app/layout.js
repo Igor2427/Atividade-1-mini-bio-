8d8bb1d4573f2b5a9bc17f0c4fc0fae244916b6f
Igor2427 <128043201+Igor2427@users.noreply.github.com>
2025-08-20 20:11:45 +0000

    mini-bio.2

--- mini_bio/app/layout.js
+++ mini_bio/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,10 +19,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
   );
+  
 }
